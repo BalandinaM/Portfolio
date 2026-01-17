8f203fd92bbdccdd66593719adf027d36ebb67ea
BalandinaM <marina.balandina.89@mail.ru>
2026-01-17 23:39:38 +0500

    добавлены пункты в меню

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,16 +1,34 @@
 import styled from "styled-components";
 import { FlexWrapper } from "../FlexWrapper.styled";
 
+const MenuItems = [
+  {
+    name: "Обо мне",
+    href: "#",
+  },
+  {
+    name: "Проекты",
+    href: "#",
+  },
+  {
+    name: "Технологический стек",
+    href: "#",
+  },
+  {
+    name: "Контакты",
+    href: "#",
+  }
+]
+
 export const Menu = () => {
   return (
     <nav>
       <FlexWrapper as="ul" $justify="center" $colgap="70px">
-        <ItemList>
-          <a href="#">Проекты</a>
-        </ItemList>
-        <ItemList>
-          <a href="#">Контакты</a>
-        </ItemList>
+        {MenuItems.map((item, index) => (
+          <ItemList key={index}>
+            <a href={item.href}>{item.name}</a>
+          </ItemList>
+        ))}
       </FlexWrapper>
     </nav>
   );
