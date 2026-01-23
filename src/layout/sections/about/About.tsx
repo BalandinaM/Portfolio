db9369c85881cc25afbd599c1ea274c2027ecbf6
BalandinaM <marina.balandina.89@mail.ru>
2026-01-23 15:05:30 +0500

    refactor about

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -1,43 +1,13 @@
-import styled from "styled-components"
 import { Container } from "../../../components/Container"
-import { Theme } from "../../../styles/Theme"
+import { S } from "./About_Styles"
 
 export const About = () => {
   return (
     <section>
       <Container>
-          <Title>Обо мне</Title>
-          <Text>Длинный курган был построен на земле, ранее заселенной в эпоху мезолита. Он состоял из земляного кургана прямоугольной формы, длина которого, по оценкам, составляла 15 метров (50 футов), с помещением, построенным из сарсеновых мегалитов на его восточном конце. В эпоху неолита в эту камеру помещали как погребенные, так и кремированные человеческие останки, представляющие собой по меньшей мере девять или десять особей.</Text>
+          <S.Title>Обо мне</S.Title>
+          <S.Text>Длинный курган был построен на земле, ранее заселенной в эпоху мезолита. Он состоял из земляного кургана прямоугольной формы, длина которого, по оценкам, составляла 15 метров (50 футов), с помещением, построенным из сарсеновых мегалитов на его восточном конце. В эпоху неолита в эту камеру помещали как погребенные, так и кремированные человеческие останки, представляющие собой по меньшей мере девять или десять особей.</S.Text>
       </Container>
     </section>
   )
 }
-
-const Title = styled.h2`
-  padding-left: 20px;
-  
-  @media ${Theme.media.mobile} {
-    margin-bottom: 20px;
-  }
-`
-
-const Text = styled.p`
-  padding-left: 20px;
-  position: relative;
-
-  &::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 5px;
-    height: 100%;
-    background: ${Theme.gradient.gradient};
-    border-radius: 5px;
-
-
-    @media ${Theme.media.mobile} {
-      height: 33.3%;
-    }
-  }
-`
---
+++ src/layout/sections/about/About_Styles.ts
@@ -0,0 +1,34 @@
+import { Theme } from "../../../styles/Theme";
+
+const Title = styled.h2`
+  padding-left: 20px;
+
+  @media ${Theme.media.mobile} {
+    margin-bottom: 20px;
+  }
+`;
+
+const Text = styled.p`
+  padding-left: 20px;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 5px;
+    height: 100%;
+    background: ${Theme.gradient.gradient};
+    border-radius: 5px;
+
+    @media ${Theme.media.mobile} {
+      height: 33.3%;
+    }
+  }
+`;
+
+export const S = {
+  Title,
+  Text,
+};
